d72cb20911dfec821f4d08209a16069b61e2860c
AlexMtzGzn <alex13141926@gmail.com>
2024-09-18 13:43:15 -0600

    Correccion de platilla

--- TSP_ANT/algoritmo_evolutivo_diferencial.c
+++ TSP_ANT/algoritmo_evolutivo_diferencial.c
@@ -1,3 +1,9 @@
 #include "algoritmo_evolutivo_diferencial.h"
 #include <stdio.h>
 #include <time.h>
+
+//Parametros ya definidos como variables globales
+int poblacion=100;
+//generaciones 50
+//tasa de mutacion 0.5
+//tasa de cruza 0.5
--- TSP_ANT/algoritmo_evolutivo_diferencial.h
+++ TSP_ANT/algoritmo_evolutivo_diferencial.h
@@ -7,10 +7,6 @@ typedef struct{
 	double * arreglo;
 	double fitness;
 }vector;
-//Parametros ya definidos como variables globales
-int poblacion=100;
-//generaciones 50
-//tasa de mutacion 0.5
-//tasa de cruza 0.5
+
 
 #endif // ALGORITMO_EVOLUTIVO_DIFERENCIAL.H
