78817818db34c9516829df80203bb7990c759b31
AlexMtzGzn <alex13141926@gmail.com>
2024-09-20 02:01:07 -0600

    Inicializacion de cada hormiga

--- TSP_ANT/tsp_ant.c
+++ TSP_ANT/tsp_ant.c
@@ -5,11 +5,16 @@
 #include "algoritmo_evolutivo_diferencial.h"
 #include "tsp_ant.h"
 
-void inializacionHormiga(){
-
+void inializacionHormiga(hormiga * hor, int tamanio_instancia,int numHormigas){
+    for(int i = 0; i < tamanio_instancia; i++){
+        hor[i].ruta = asignacion_memoria_arreglo(tamanio_instancia+1);
+        hor[i].tabu = asignacion_memoria_arreglo(tamanio_instancia);
+        hor[i].fitness = 0.0;
+    }
+    printf("Acabe de inicializar");
 }
 
 void tsp_ant(individuo * ind, double ** instancia_feromona,double ** instancia_distancias,int tamanio_instancia){
-
-    
+    hormiga * hor = asignar_memoria_arreglo_estructura_hormiga(ind->numHormigas);
+    inializacionHormiga(hor,tamanio_instancia,ind->numHormigas);
 }
