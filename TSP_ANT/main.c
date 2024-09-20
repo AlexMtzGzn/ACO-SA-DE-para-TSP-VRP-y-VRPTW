9e396d17b036a9e3f74d1330e18cc77e32b1427c
AlexMtzGzn <alex13141926@gmail.com>
2024-09-19 21:41:50 -0600

    Ajuste de poblacion

--- TSP_ANT/main.c
+++ TSP_ANT/main.c
@@ -4,15 +4,14 @@
 #include <string.h>
 #include "algoritmo_evolutivo_diferencial.h"
 
-void tsp(int poblacion, int tamanio_instancia,char * instancia){
-    a_v_d(poblacion,tamanio_instancia,instancia);  
+void tsp(int tamanio_instancia,char * instancia){
+    a_v_d(tamanio_instancia,instancia);  
 }
 
 int main (int argc, char * argv[]){
-    int poblacion = atoi(argv[1]);
-    int tamanio_instancia = atoi(argv[2]);
+    int tamanio_instancia = atoi(argv[1]);
     char instancia[20];
-    strcpy(instancia,argv[3]);
-    tsp(poblacion,tamanio_instancia,instancia); 
+    strcpy(instancia,argv[2]);
+    tsp(tamanio_instancia,instancia); 
     return 0;
 }
