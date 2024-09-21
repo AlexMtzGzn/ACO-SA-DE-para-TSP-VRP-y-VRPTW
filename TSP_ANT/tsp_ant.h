14ddd21021036b7af83af143efd4d9ba151ca1b1
AlexMtzGzn <alex13141926@gmail.com>
2024-09-21 01:43:35 -0600

    Prototipo actualizar_feromona

--- TSP_ANT/tsp_ant.h
+++ TSP_ANT/tsp_ant.h
@@ -7,6 +7,7 @@ typedef struct
     double fitness;
 }hormiga;
 
+void actualizar_feromonas(double ** instacia_feromona, hormiga *hor, individuo * ind, int tamanio_instancia);
 void caminar_hormiga(hormiga *hor, double **instancia_feromona, double **probabilidad, double **visibilidad, int tamanio_instancia);
 void inicializar_ruleta(double ** instancia_feromona,double ** instacia_distancias, double **visibilidad, double **probabilidad, hormiga * hor, individuo * ind, int tamanio_instancia);
 void ant_system(hormiga *hor, individuo *ind, double **instancia_distancias, double **instansia_feromona,double ** probabilidad, double ** visibilidad, int tamanio_instancia);
