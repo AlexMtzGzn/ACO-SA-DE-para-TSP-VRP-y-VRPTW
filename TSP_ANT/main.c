b568747820bd9a708b32fc9f346dd1d08c068214
AlexMtzGzn <alex13141926@gmail.com>
2024-09-21 01:21:29 -0600

    Ajuste de funciones en el main

--- TSP_ANT/main.c
+++ TSP_ANT/main.c
@@ -4,14 +4,11 @@
 #include <string.h>
 #include "algoritmo_evolutivo_diferencial.h"
 
-void tsp(int tamanio_instancia,char * archivo_instancia){
-    a_v_d(tamanio_instancia,archivo_instancia);  
-}
-
-int main (int argc, char * argv[]){
+int main(int argc, char *argv[])
+{
     int tamanio_instancia = atoi(argv[1]);
     char archivo_instancia[20];
-    strcpy(archivo_instancia,argv[2]);
-    tsp(tamanio_instancia,archivo_instancia); 
+    strcpy(archivo_instancia, argv[2]);
+    a_v_d(tamanio_instancia, archivo_instancia);
     return 0;
 }
