833749c421ca40941bb1220d4f875e519dbc44f6
AlexMtzGzn <alex13141926@gmail.com>
2024-09-21 15:16:25 -0600

    Prototipos para liberar memoria

--- TSP_ANT/control_memoria.h
+++ TSP_ANT/control_memoria.h
@@ -10,4 +10,8 @@ individuo *asignar_memoria_arreglo_estructura_individuo(int poblacion);
 hormiga * asignar_memoria_arreglo_estructura_hormiga(int numHormigas);
 double **asignacion_memoria_instancia(int tamanio_instancia);
 double * asignacion_memoria_arreglo(int tamanio_instancia);
+void liberar_memoria_arreglo_estructura_individuo(individuo *ind);
+void liberar_memoria_arreglo_estructura_hormiga(hormiga *hor);
+void liberar_memoria_instancia(double **matriz, int tamanio_instancia);
+void liberar_memoria_arreglo(double *arreglo);
 #endif // CONTROL_MEMORIA_H
