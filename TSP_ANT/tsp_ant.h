b542e81298f1dcfc36dca3e250ccda0deead7820
AlexMtzGzn <alex13141926@gmail.com>
2024-09-21 01:42:56 -0600

    Prototipo caminar_hormiga

--- TSP_ANT/tsp_ant.h
+++ TSP_ANT/tsp_ant.h
@@ -7,6 +7,7 @@ typedef struct
     double fitness;
 }hormiga;
 
+void caminar_hormiga(hormiga *hor, double **instancia_feromona, double **probabilidad, double **visibilidad, int tamanio_instancia);
 void inicializar_ruleta(double ** instancia_feromona,double ** instacia_distancias, double **visibilidad, double **probabilidad, hormiga * hor, individuo * ind, int tamanio_instancia);
 void ant_system(hormiga *hor, individuo *ind, double **instancia_distancias, double **instansia_feromona,double ** probabilidad, double ** visibilidad, int tamanio_instancia);
 void inializacionHormiga(hormiga *hor, int tamanio_instancia, int numHormigas);
