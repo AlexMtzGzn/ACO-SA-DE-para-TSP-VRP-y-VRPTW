9019d529f91534751ebc410e3c2ff3e8d7335eea
AlexMtzGzn <alex13141926@gmail.com>
2024-09-21 01:28:07 -0600

    Ajustes en los paramtros del main

--- TSP_ANT/main.c
+++ TSP_ANT/main.c
@@ -6,9 +6,9 @@
 
 int main(int argc, char *argv[])
 {
-    int tamanio_instancia = atoi(argv[1]);
+    int tamanio_instancia = atoi(argv[3]),poblacion=atoi(argv[1]),generaciones = atoi(argv[2]);
     char archivo_instancia[20];
-    strcpy(archivo_instancia, argv[2]);
+    strcpy(archivo_instancia, argv[4]);
     algoritmo_evolutivo_diferencial(tamanio_instancia, archivo_instancia);
     return 0;
 }
