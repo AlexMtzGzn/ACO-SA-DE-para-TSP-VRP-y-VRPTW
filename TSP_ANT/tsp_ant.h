e026485fdda26fc32ed90d2db51521917a86afc2
AlexMtzGzn <alex13141926@gmail.com>
2024-09-22 13:50:25 -0600

    Hacer de nuevo

--- TSP_ANT/tsp_ant.h
+++ TSP_ANT/tsp_ant.h
@@ -7,15 +7,9 @@ typedef struct
     double fitness;
 }hormiga;
 
-double mejor_fitness(hormiga * hor, int numHormigas);
-void actualizar_feromonas(double ** instacia_feromona, hormiga *hor, individuo * ind, int tamanio_instancia);
-double calcular_distancia (hormiga * hor, double ** instancia_distancias, int tamanio_instancia);
-void caminar_hormiga(hormiga *hor, double **instancia_feromona, double **probabilidad, double **visibilidad, int tamanio_instancia);
-void inicializar_ruleta(double ** instancia_feromona,double ** instacia_distancias, double **visibilidad, double **probabilidad, hormiga * hor, individuo * ind, int tamanio_instancia);
 void ant_system(hormiga *hor, individuo *ind, double **instancia_distancias, double **instansia_feromona,double ** probabilidad, double ** visibilidad, int tamanio_instancia);
 void inicializar_visibilidad(double ** instancia_visibilidad,double ** instancia_distancias, int tamanio_instancia);
 void inializacionHormiga(hormiga *hor, int tamanio_instancia, int numHormigas);
-void imprimir_rutas_hormigas(hormiga *hor, int numHormigas, int tamanio_instancia);
 double aco_tsp(individuo * ind, double ** instancia_feromona,double ** instancia_distancias,int tamanio_instancia);
 
 #endif // TSP_ANT_H
