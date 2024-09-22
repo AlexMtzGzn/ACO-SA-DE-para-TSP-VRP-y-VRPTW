bd0f2f78e7054f90f45159a50b5b13cbec6e0c61
AlexMtzGzn <alex13141926@gmail.com>
2024-09-22 17:01:33 -0600

    Declaracion de prototipos de calcularcosto y actualizar feromona

--- TSP_ANT/tsp_ant.h
+++ TSP_ANT/tsp_ant.h
@@ -6,8 +6,11 @@ typedef struct
     int *tabu;
     double *probabilidades;
     double *fitness;
+    
 }hormiga;
 
+void calcular_costo(hormiga *hor, double **instancia_distancias, int tamanio_instancia);
+void actualizar_feromona(hormiga *hor, individuo *ind, double **instancia_distancias, double **instancia_feromona, int tamanio_instancia);
 void ruta_hormiga(hormiga * hor,individuo * ind, double ** instancia_distancia, double ** instancia_feromona, double ** instancia_visibilidad, int tamanio_instancia);
 void ant_system(hormiga *hor, individuo *ind, double **instancia_distancias, double **instansia_feromona,double ** instancia_visibilidad, int tamanio_instancia);
 void inicializar_visibilidad(double ** instancia_visibilidad,double ** instancia_distancias, int tamanio_instancia);
