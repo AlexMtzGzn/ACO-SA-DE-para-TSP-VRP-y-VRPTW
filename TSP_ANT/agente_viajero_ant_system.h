74bf45f2a0d85d197fe02f839ea5a84eadb43411
AlexMtzGzn <alex13141926@gmail.com>
2024-09-29 12:13:00 -0600

    Iplementacion para agente viajero

---
+++ TSP_ANT/agente_viajero_ant_system.h
@@ -0,0 +1,13 @@
+#ifndef AGENTE_VIAJERO_ANT_SYSTEM_H
+#define AGENTE_VIAJERO_ANT_SYSTEM_H
+
+typedef struct
+{
+    double finess;           
+    double *posibilidades;  
+    int *tabu;
+    int * ruta;
+}hormiga;
+
+
+#endif // AGENTE_VIAJERO_ANT_SYSTEM_H
