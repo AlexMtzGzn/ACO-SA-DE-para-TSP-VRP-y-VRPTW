9c7949a5ba87ce64e20fee10266bec11b96ba250
AlexMtzGzn <alex13141926@gmail.com>
2024-10-25 20:44:43 -0600

    ajuste del recocido simulado

--- TSP-SA-AED/src/TSP_SA.c
+++ TSP-SA-AED/src/TSP_SA.c
@@ -1,18 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "TSP_SA.h"
 #include "AED.h"
 #include "entrada_salida_datos.h"
 #include "control_memoria.h"
 
-void generaSolInicial(int *solucion_inicial, int tamanio_instacia){
-
+void evaluaFO(solucion *solucion_inicial, double **instancia_distancias, int tamanio_instancia)
+{
+    solucion_inicial->fitness = 0.0;
+    for (int i = 0; i < tamanio_instancia - 1; ++i)
+    {
+        solucion_inicial->fitness += instancia_distancias[solucion_inicial->solucion[i]][solucion_inicial->solucion[i + 1]];
+    }
+    solucion_inicial->fitness += instancia_distancias[solucion_inicial->solucion[tamanio_instancia - 1]][solucion_inicial->solucion[0]]; // regreso a la ciudad inicial
 }
 
-void tsp_sa(individuo *ind , double ** instancias_distancias, int tamanio_instancia){
+void copiaSolucion(solucion *solucion_original, solucion *solucion_copia, int tamanio_instancia)
+{
+    for (int i = 0; i < tamanio_instancia; ++i)
+        solucion_copia->solucion[i] = solucion_original->solucion[i];
+    solucion_copia->fitness = solucion_original->fitness;
+}
 
-    int * solucion_inicial=asignar_memoria_ruta(tamanio_instancia+1);
-    int * solucion_vecina=asignar_memoria_ruta(tamanio_instancia+1);
-    generaSolInicial(solucion_inicial,tamanio_instancia);
+void intercambiarCiudades(solucion *solucion, int tamanio_instancia)
+{
+    int ciudad1 = rand() % tamanio_instancia;
+    int ciudad2 = rand() % tamanio_instancia;
+    int temp = solucion->solucion[ciudad1];
+    solucion->solucion[ciudad1] = solucion->solucion[ciudad2];
+    solucion->solucion[ciudad2] = temp;
+}
 
+void generaVecino(solucion *solucion_inicial, solucion *solucion_vecina, int tamanio_instancia)
+{
+    copiaSolucion(solucion_inicial, solucion_vecina, tamanio_instancia);
+    intercambiarCiudades(solucion_vecina, tamanio_instancia);
+}
+
+void sa(individuo *ind, solucion *solucion_inicial, solucion *solucion_vecina, solucion *mejor_solucion, double **instancias_distancias, int tamanio_instancia)
+{
+    evaluaFO(solucion_inicial, instancias_distancias, tamanio_instancia);
+    copiaSolucion(solucion_inicial, mejor_solucion, tamanio_instancia);
+
+    double temperatura = ind->temperatura_inicial;
+    double mejorFO = mejor_solucion->fitness;
+
+    while (temperatura > ind->temperatura_final)
+    {
+        for (int i = 0; i < ind->numIteraciones; ++i)
+        {
+            generaVecino(solucion_inicial, solucion_vecina, tamanio_instancia);
+            evaluaFO(solucion_vecina, instancias_distancias, tamanio_instancia);
+            double delta = solucion_vecina->fitness - solucion_inicial->fitness;
+
+            if (delta <= 0 || (rand() / (double)RAND_MAX) < exp(-delta / temperatura))
+            {
+                copiaSolucion(solucion_vecina, solucion_inicial, tamanio_instancia);
+            }
+
+            if (solucion_inicial->fitness < mejorFO)
+            {
+                copiaSolucion(solucion_inicial, mejor_solucion, tamanio_instancia);
+                mejorFO = mejor_solucion->fitness;
+            }
+        }
+        temperatura *= ind->enfriamiento;
+    }
+
+    printf("Mejor distancia encontrada: %f\n", mejorFO);
+    printf("Mejor ruta: ");
+    for (int i = 0; i < tamanio_instancia; ++i)
+    {
+        printf("%d ", mejor_solucion->solucion[i]);
+    }
+    printf("\n");
+}
+
+void generaSolInicial(solucion *solucion_inicial, int tamanio_instancia)
+{
+    for (int i = 0; i < tamanio_instancia; ++i)
+        solucion_inicial->solucion[i] = i;
+
+    for (int i = 0; i < tamanio_instancia; ++i)
+    {
+        int j = rand() % tamanio_instancia;
+        int temp = solucion_inicial->solucion[i];
+        solucion_inicial->solucion[i] = solucion_inicial->solucion[j];
+        solucion_inicial->solucion[j] = temp;
+    }
+}
+
+void tsp_sa(individuo *ind, double **instancias_distancias, int tamanio_instancia)
+{
+    solucion *solucion_inicial = (solucion *)malloc(sizeof(solucion));
+    solucion *solucion_vecina = (solucion *)malloc(sizeof(solucion));
+    solucion *mejor_solucion = (solucion *)malloc(sizeof(solucion));
+
+    solucion_inicial->solucion = asignar_memoria_ruta(tamanio_instancia);
+    solucion_vecina->solucion = asignar_memoria_ruta(tamanio_instancia);
+    mejor_solucion->solucion = asignar_memoria_ruta(tamanio_instancia);
+
+    generaSolInicial(solucion_inicial, tamanio_instancia);
+    sa(ind, solucion_inicial, solucion_vecina, mejor_solucion, instancias_distancias, tamanio_instancia);
+
+    // Liberación de memoria
+    free(solucion_inicial->solucion);
+    free(solucion_vecina->solucion);
+    free(mejor_solucion->solucion);
+    free(solucion_inicial);
+    free(solucion_vecina);
+    free(mejor_solucion);
 }
--- TSP-SA-AED/src/TSP_SA.h
+++ TSP-SA-AED/src/TSP_SA.h
@@ -1,7 +1,15 @@
 #ifndef TSP_SA_H
 #define TSP_SA_H
 #include "AED.h"
-void generaSolInicial(int *solucion_inicial, int tamanio_instacia);
+
+typedef struct{
+    int *solucion;
+    double fitness;
+} solucion;
+
+void evaluaFO(solucion *solucion_inicial, double **instancia_distancias, int tamanio_instancia);
+void sa(individuo * ind,solucion * solucion_incial,solucion * solucion_vecina,solucion * mejor_solucion,double ** instancias_distancias,int tamanio_instancia);
+void generaSolucion(solucion *solucion, int tamanio_instacia);
 void tsp_sa(individuo *ind , double ** instancias_distancias, int tamanio_instancia);
 
 #endif // TSP_SH_H
