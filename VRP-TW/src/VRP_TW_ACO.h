e84dae9efd39cef48e42a821bc5b6af8ec49556d
amg <alex13141926@gmail.com>
2025-03-02 20:15:53 -0600

    Creamos estructura vehiculo_hormiga

--- VRP-TW/src/VRP_TW_ACO.h
+++ VRP-TW/src/VRP_TW_ACO.h
@@ -1,6 +1,12 @@
 #ifndef VRP_TW_ACO_H
 #define VRP_TW_ACO_H
 
-
+typedef struct vehiculo_hormiga{
+    int id;
+    double capacidad;
+    double capacidad_restante;
+    double timpo_comsumido;
+    double timpo_maximo;
+};
 
 #endif // VRP_TW_ACO_H
