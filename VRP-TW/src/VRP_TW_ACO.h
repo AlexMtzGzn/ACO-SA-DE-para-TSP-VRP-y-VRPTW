c77ef19585dab5778a60fe5b424c69556b193b70
amg <alex13141926@gmail.com>
2025-03-04 12:56:56 -0600

    ajuste de ortografia y cambio de nombre

--- VRP-TW/src/VRP_TW_ACO.h
+++ VRP-TW/src/VRP_TW_ACO.h
@@ -1,29 +1,25 @@
 #ifndef VRP_TW_ACO_H
 #define VRP_TW_ACO_H
 
-
 #include "AED.h"
 #include "vrp_tw_setting.h"
 #include "entrada_salida_datos.h"
 #include "control_memoria.h"
 
-typedef struct vehiculo
-{
+typedef struct vehiculo {
     int id_vehiculo;
     double capacidad;
     double capacidad_restante;
-    double timpo_comsumido;
-    double timpo_maximo;
+    double tiempo_consumido;
+    double tiempo_maximo;
     int clientes;
     int *ruta;
-};
+} vehiculo;
 
-
-typedef struct hormiga{
+typedef struct hormiga {
     int *tabu;
     int vehiculos;
-    struct vehiculo *vehiculo;
-    
-};
+    vehiculo *flota;  
+} hormiga;
 
 #endif // VRP_TW_ACO_H
