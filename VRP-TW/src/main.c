de378dc4b4dd5a218625aab9f172afab7e51f3f3
amg <alex13141926@gmail.com>
2025-03-04 20:08:56 -0600

    agregamos una nueva funcion

--- VRP-TW/src/main.c
+++ VRP-TW/src/main.c
@@ -3,13 +3,24 @@
 #include <string.h>
 #include "aed.h"
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-    int num_poblacion = atoi(argv[1]); //Ingresa el tamaño de la poblacion
-    int num_generaciones = atoi(argv[2]); //Ingresa el numero de generacones
-    char archivo_instancia[100]; //Declaramos nombre de la instacia a leer
-    snprintf(archivo_instancia, sizeof(archivo_instancia), "%s", argv[3]); //Copiamos el nombre de la instacia a leer a del parametro argv[3];
-    //aed_vrp_tw(num_poblacion, num_generaciones, archivo_instancia); //Enviamos al como parametro el numero de la poblacion , numero de generaciones , y el archivo de la intsncia
+    int num_poblacion = atoi(argv[1]);    // Ingresa el tamaño de la poblacion
+    int num_generaciones = atoi(argv[2]); // Ingresa el numero de generacones
+    if (num_poblacion < 3)
+    {
+        do
+        {
+
+            printf("\n La poblacion debe ser mayo a 2 !!!\nIngresa de nuevo el valor de la poblacion: ");
+            scanf("%i", &num_poblacion);
+
+        } while (num_poblacion < 3);
+    }
+    char archivo_instancia[100];                                           // Declaramos nombre de la instacia a leer
+    snprintf(archivo_instancia, sizeof(archivo_instancia), "%s", argv[3]); // Copiamos el nombre de la instacia a leer a del parametro argv[3];
+    aed_vrp_tw(num_poblacion, num_generaciones, archivo_instancia);        // Enviamos al como parametro el numero de la poblacion , numero de generaciones , y el archivo de la intsncia
 
     return 0;
-} 
+}
--- VRP-TW/src/vrp_tw_aco.c
+++ VRP-TW/src/vrp_tw_aco.c
@@ -5,7 +5,7 @@
 #include "vrp_tw_aco.h"
 #include "control_memoria.h"
 
-/*void imprimir_info_hormiga(struct hormiga *h, int numHormiga)
+void imprimir_info_hormiga(struct hormiga *h, int numHormiga)
 {
    printf("Hormiga %d:\n", numHormiga);
    printf("  Número de vehículos usados: %d\n", h->vehiculos_contados);
@@ -13,9 +13,9 @@
    for (int i = 0; i < h->vehiculos_contados; i++)
    {
       printf("  Vehículo %d:\n", h->flota[i].id_vehiculo);
-      printf("    Capacidad: %d\n", h->flota[i].capacidad);
-      printf("    Capacidad restante: %d\n", h->flota[i].capacidad_restante);
-      printf("    Tiempo consumido: %.2f\n", h->flota[i].tiempo_consumido);
+      printf("    Capacidad: %.2lf\n", h->flota[i].capacidad);
+      printf("    Capacidad restante: %.2lf\n", h->flota[i].capacidad_restante);
+      printf("    Tiempo consumido: %.2lf\n", h->flota[i].tiempo_consumido);
       printf("    Clientes atendidos: %d\n", h->flota[i].clientes_contados);
       printf("    Ruta: ");
 
@@ -25,8 +25,11 @@
       }
       printf("\n");
    }
-   printf("\n");
-}*/
+}
+
+void aco(){
+
+}
 
 void agregar_cliente_a_ruta(struct vehiculo *vehiculo, int nuevo_cliente)
 {
@@ -42,30 +45,37 @@ void inicializar_hormigas_vehiculos(struct vrp_configuracion *vrp, struct indivi
    for (int i = 0; i < ind->numHormigas; i++)
    {
       hormiga[i].tabu = asignar_memoria_arreglo(vrp->num_clientes);
+      for(int j = 0; j < vrp->num_clientes;j++) hormiga[i].tabu[i] = 0;
+      hormiga[i].tabu[0]=1;
       hormiga[i].vehiculos_contados = 1;
       hormiga[i].vehiculos_maximos = vrp->num_vehiculos;
-      hormiga[i].id_hormiga = i + 1;
+
+
+      hormiga[i].id_hormiga = i;
 
       hormiga[i].flota = asignar_memoria_vehiculo(vrp);
-      hormiga[i].flota[0].id_vehiculo = i + 1;
+      hormiga[i].flota[0].id_vehiculo = 0;
       hormiga[i].flota[0].capacidad = vrp->num_capacidad;
       hormiga[i].flota[0].capacidad_restante = vrp->num_capacidad;
       hormiga[i].flota[0].tiempo_consumido = 0.0;
       hormiga[i].flota[0].tiempo_maximo = 0.0;
-      hormiga[i].flota[0].clientes_contados = 1;
-      hormiga[i].flota[0].ruta = asignar_memoria_arreglo(1);
+      hormiga[i].flota[0].clientes_contados = 0;
+      hormiga[i].flota[0].ruta = NULL;
       agregar_cliente_a_ruta(&hormiga[i].flota[0], vrp->clientes[0].id_cliente);
    }
 }
 
-void vrp_tw_aco(vrp_configuracion *vrp, individuo *ind, double **instancia_visiblidad, double **instancia_feromona)
+void vrp_tw_aco(struct vrp_configuracion *vrp,struct individuo *ind, double **instancia_visiblidad, double **instancia_feromona)
 {
 
    struct hormiga *hormiga = asignar_memoria_hormiga(ind);
    inicializar_hormigas_vehiculos(vrp, ind, hormiga);
 
-   /*for (int i = 0; i < ind->numHormigas; i++)
+
+   for (int i = 0; i < ind->numHormigas; i++)
    {
       imprimir_info_hormiga(&hormiga[i], i + 1);
-   }*/
+     
+   }
+   printf("\n\n\n");
 }
