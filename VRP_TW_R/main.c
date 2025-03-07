c17872b8ee766f7dcab93a3344f434fad77d1a8c
amg <alex13141926@gmail.com>
2025-03-06 22:22:20 -0600

    Vamos a restaurar el codigo con listas creadas e mi repo

---
+++ VRP_TW_R/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+
+    int num_poblacion = atoi(argv[1]);    // Ingresa el tamaño de la poblacion
+    int num_generaciones = atoi(argv[2]); // Ingresa el numero de generacones
+
+    /*Aqui estamos verificando que el numero de la poblacion no sea menor a 3*/
+    if (num_poblacion < 3)
+    {
+        do
+        {
+
+            printf("\nLa poblacion debe ser mayor a 2 !!!\nIngresa de nuevo el valor de la poblacion: ");
+            scanf("%i", &num_poblacion);
+
+        } while (num_poblacion < 3);
+    }
+    char archivo_instancia[100];                                           // Declaramos nombre de la instacia a leer
+    snprintf(archivo_instancia, sizeof(archivo_instancia), "%s", argv[3]); // Copiamos el nombre de la instacia a leer a del parametro argv[3];
+
+    return 0;
+}
