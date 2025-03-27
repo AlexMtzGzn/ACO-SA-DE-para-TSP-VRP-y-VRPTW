059a934351f80b2f318422418dea8b92153ab79b
amg <alex13141926@gmail.com>
2025-03-26 21:46:14 -0600

    Vamos ajustar el codigo en limpio

--- VRP_TW_R/src/vrp_tw_aco.h
+++ VRP_TW_R/src/vrp_tw_aco.h
@@ -16,7 +16,7 @@ typedef struct vehiculo
     double capacidad_maxima;
     double capacidad_acumulada;
     double vt_actual; // Timepo Actual, ajustar despues
-    double vt_final;    // Tiemo final de la venta
+    double vt_final;  // Tiemo final de la venta
     double vt_inicial;
     double velocidad;
     int clientes_contados;
@@ -37,6 +37,7 @@ typedef struct hormiga
     double umbral;
     int vehiculos_necesarios;
     int vehiculos_maximos;
+
     double fitness_global;
     lista_vehiculos *flota;
 } hormiga;
@@ -46,13 +47,14 @@ void imprimir_vehiculo(struct vehiculo *vehiculo);
 void imprimir_flota(struct lista_vehiculos *flota);
 void imprimir_tabu(int *tabu, int num_clientes);
 void imprimir_hormigas(struct hormiga *hormigas, struct vrp_configuracion *vrp, int num_hormigas);
-void actualizar_feromona(struct individuo *ind, struct hormiga *hormiga, struct vrp_configuracion *vrp, double **instancia_feromona,double delta);
+void actualizar_feromona(struct individuo *ind, struct hormiga *hormiga, struct vrp_configuracion *vrp, double **instancia_feromona, double delta);
 void calcular_fitness(struct hormiga *hormiga, double **instancia_distancias);
 void inicializar_hormiga(struct vrp_configuracion *vrp, struct individuo *ind, struct hormiga *hormiga);
 void calcular_posibles_clientes(int origen, struct vehiculo *vehiculo, struct vrp_configuracion *vrp, struct hormiga *hormiga, double **instancia_distancias);
-double calcular_probabilidad(int origen, int destino, struct individuo *ind, struct vrp_configuracion *vrp, struct hormiga *hormiga, double **instancia_feromona, double **instancia_visibilidad,double **instancia_ventanas_tiempo);
-void aco(struct vrp_configuracion *vrp, struct individuo *ind, struct hormiga *hormiga, double **instancia_visiblidad, double **instancia_feromona, double **instancia_distancias,double **instancia_ventanas_tiempo);
+double calcular_probabilidad(int origen, int destino, struct individuo *ind, struct vrp_configuracion *vrp, struct hormiga *hormiga, double **instancia_feromona, double **instancia_visibilidad, double **instancia_ventanas_tiempo);
+void aco(struct vrp_configuracion *vrp, struct individuo *ind, struct hormiga *hormiga, double **instancia_visiblidad, double **instancia_feromona, double **instancia_distancias, double **instancia_ventanas_tiempo);
 void liberar_memoria_hormiga(struct hormiga *hormiga, struct individuo *ind);
 void reiniciar_hormiga(struct hormiga *hormiga, struct vrp_configuracion *vrp);
-void vrp_tw_aco(struct vrp_configuracion *vrp, struct individuo *ind, double **instancia_visiblidad, double **instancia_distancias, double **instancia_feromona,double **instancia_ventanas_tiempo);
+void vrp_tw_aco(struct vrp_configuracion *vrp, struct individuo *ind, double **instancia_visiblidad, double **instancia_distancias, double **instancia_feromona, double **instancia_ventanas_tiempo);
+
 #endif // VRP_TW_ACO_H
