abe79736a8006ed8bf06b6fb331ef8890ae7f20c
amg <alex13141926@gmail.com>
2025-04-02 03:51:57 -0600

    comentamos prototipos de configuracion vrp tw .h

--- VRP_TW/includes/configuracion_vrp_tw.h
+++ VRP_TW/includes/configuracion_vrp_tw.h
@@ -1,12 +1,21 @@
 #ifndef CONFIGURACION_VRP_TW_H
 #define CONFIGURACION_VRP_TW_H
 
+// Incluimos las bibliotecas necesarias para trabajar con las estructuras de datos del VRP
 #include "../includes/estructuras.h"
 
-// Declaramos prototipos
+// Declaramos prototipos de funciones que gestionan la configuración de la instancia VRP con ventanas de tiempo (VRPTW)
+
+// Función para leer una instancia de VRP desde un archivo CSV, llenando la estructura vrp_configuracion
 void leemos_csv(vrp_configuracion *vrp, char *archivo_instancia);
+
+// Función para crear un archivo CSV a partir de los datos de la estructura vrp_configuracion
 void creamos_csv(vrp_configuracion *vrp, char *archivo_instancia);
+
+// Función para leer una instancia de VRP desde un archivo de texto, llenando la estructura vrp_configuracion
 void leemos_txt(vrp_configuracion *vrp, char *ruta);
+
+// Función para leer una instancia de VRP desde un archivo de instancia dado, devolviendo un puntero a la configuración VRP
 vrp_configuracion *leer_instancia(char *archivo_instancia);
 
 #endif // CONFIGURACION_VRP_TW_H
