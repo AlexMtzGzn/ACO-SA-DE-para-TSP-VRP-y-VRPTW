9f7e04fe189185f29901ada8fc77b1ee7d8c9657
amg <alex13141926@gmail.com>
2025-04-02 21:39:15 -0600

    comentamos

--- VRP_TW/includes/configuracion_vrp_tw.h
+++ VRP_TW/includes/configuracion_vrp_tw.h
@@ -2,9 +2,8 @@
 #define CONFIGURACION_VRP_TW_H
 
 // Incluimos las bibliotecas necesarias para trabajar con las estructuras de datos del VRP
-#include "../includes/estructuras.h"
 #include <cjson/cJSON.h>
-
+#include "../includes/estructuras.h"
 
 // Declaramos prototipos de funciones que gestionan la configuración de la instancia VRP con ventanas de tiempo (VRPTW)
 
@@ -20,10 +19,16 @@ void leemos_txt(vrp_configuracion *vrp, char *ruta);
 // Función para leer una instancia de VRP desde un archivo de instancia dado, devolviendo un puntero a la configuración VRP
 vrp_configuracion *leer_instancia(char *archivo_instancia);
 
+// Función para generar un objeto JSON con las coordenadas de la ruta
 cJSON *generar_ruta_coordenadas(lista_ruta *ruta, cliente *clientes);
-cJSON *vehiculo_a_json(vehiculo *v, cliente *clientes);
-cJSON *individuo_a_json(individuo *ind, cliente *clientes);
-void guardar_json_en_archivo(individuo *ind, vrp_configuracion *vrp, char *archivo_instancia);
 
+// Función para convertir un vehículo en formato JSON
+cJSON *vehiculo_a_json(vehiculo *v, cliente *clientes);
+
+// Función para convertir un individuo en formato JSON
+cJSON *individuo_a_json(individuo *ind, cliente *clientes);
+
+// Función para guardar un archivo JSON con la representación del individuo
+void guardar_json_en_archivo(individuo *ind, vrp_configuracion *vrp, char *archivo_instancia);
 
 #endif // CONFIGURACION_VRP_TW_H
