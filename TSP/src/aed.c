4fa6d090edfbe58e10753d6ef111245fabea1cca
amg <alex13141926@gmail.com>
2025-04-22 20:52:41 -0600

    Ajustams

--- TSP/src/aed.c
+++ TSP/src/aed.c
@@ -254,24 +254,51 @@ void inicializaPoblacion(struct individuo *objetivo, struct tsp_configuracion *t
       // Asigna rangos para instancias con más de 25 y hasta 51 clientes
       if (tsp->num_clientes > 25 && tsp->num_clientes <= 51)
       {
-         rango->maxAlpha = 4.0;
-         rango->minAlpha = 2.0;
-         rango->maxBeta = 6.0;
-         rango->minBeta = 2.5;
-         rango->maxRho = 0.5;
+         rango->maxAlpha = 6.0;
+         rango->minAlpha = 1.0;
+         
+         rango->maxBeta = 8.0;
+         rango->minBeta = 1.0;
+         
+         rango->maxRho = 0.9;
          rango->minRho = 0.1;
-         rango->maxNumHormigas = 40;
-         rango->minNumHormigas = 20;
-         rango->maxNumIteracionesACO = 200;
-         rango->minNumIteracionesACO = 50;
-         rango->maxTemperatura_inicial = 800.0;
-         rango->minTemperatura_inicial = 600.0;
-         rango->maxTemperatura_final = 0.1;
-         rango->minTemperatura_final = 0.01;
-         rango->maxFactor_enfriamiento = 0.99;
-         rango->minFactor_enfriamiento = 0.95;
-         rango->maxIteracionesSA = 80;
+         
+         rango->maxNumHormigas = 50;
+         rango->minNumHormigas = 5;
+         
+         rango->maxNumIteracionesACO = 500;
+         rango->minNumIteracionesACO = 30;
+         
+         rango->maxTemperatura_inicial = 2000.0;
+         rango->minTemperatura_inicial = 100.0;
+         
+         rango->maxTemperatura_final = 0.5;
+         rango->minTemperatura_final = 0.001;
+         
+         rango->maxFactor_enfriamiento = 0.999;
+         rango->minFactor_enfriamiento = 0.90;
+         
+         rango->maxIteracionesSA = 300;
          rango->minIteracionesSA = 50;
+         
+         // rango->maxAlpha = 4.0;
+         // rango->minAlpha = 2.0;
+         // rango->maxBeta = 6.0;
+         // rango->minBeta = 2.5;
+         // rango->maxRho = 0.5;
+         // rango->minRho = 0.1;
+         // rango->maxNumHormigas = 40;
+         // rango->minNumHormigas = 20;
+         // rango->maxNumIteracionesACO = 200;
+         // rango->minNumIteracionesACO = 50;
+         // rango->maxTemperatura_inicial = 800.0;
+         // rango->minTemperatura_inicial = 600.0;
+         // rango->maxTemperatura_final = 0.1;
+         // rango->minTemperatura_final = 0.01;
+         // rango->maxFactor_enfriamiento = 0.99;
+         // rango->minFactor_enfriamiento = 0.95;
+         // rango->maxIteracionesSA = 80;
+         // rango->minIteracionesSA = 50;
       }
 
       // Asigna rangos para instancias con más de 51 y hasta 101 clientes
