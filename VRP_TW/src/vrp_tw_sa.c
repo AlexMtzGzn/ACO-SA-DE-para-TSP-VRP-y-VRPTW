6ca28c9f8008cf5f9f069316c6ecb60073fbf192
AlexMtzGzn <alex13141926@gmail.com>
2025-06-03 21:36:24 -0600

    Vamos ajustar

--- VRP_TW/src/vrp_tw_sa.c
+++ VRP_TW/src/vrp_tw_sa.c
@@ -556,10 +556,10 @@ void sa(struct vrp_configuracion *vrp, struct individuo *ind, double **instancia
     while (temperatura > ind->temperatura_final)
     {
         // >>> Imprime la temperatura y los valores de fitness en cada paso del enfriamiento
-        // printf("Temp: %.4f | Fitness actual: %.4f | Mejor: %.4f\n",
-        //        temperatura,
-        //        ind->metal->fitness_solucion_actual,
-        //        ind->metal->fitness_mejor_solucion);
+        printf("Temp: %.4f | Fitness actual: %.4f | Mejor: %.4f\n",
+               temperatura,
+               ind->metal->fitness_solucion_actual,
+               ind->metal->fitness_mejor_solucion);
         // <<<
 
         for (int i = 0; i < ind->numIteracionesSA; i++)
