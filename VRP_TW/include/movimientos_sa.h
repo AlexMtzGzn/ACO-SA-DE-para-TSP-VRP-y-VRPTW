a70881495b950d946effc31991f9e0923f9e730e
AlexMtzGzn <alex13141926@gmail.com>
2025-06-04 21:52:53 -0600

    Creamos la cabecera movimientos sa paa ahi almacenar todos los movimeitnos

---
+++ VRP_TW/include/movimientos_sa.h
@@ -0,0 +1,8 @@
+#ifndef MOVIMIENTOS_SA_H
+#define MOVIMIENTOS_SA_H
+
+// Incluimos las bibliotecas necesarias para trabajar con las estructuras de datos del VRPTW
+#include "../include/estructuras.h"
+
+
+#endif // MOVIMIENTOS_SA_H
