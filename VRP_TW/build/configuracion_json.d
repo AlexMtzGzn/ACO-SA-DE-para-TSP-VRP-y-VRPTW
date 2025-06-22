56a67e58525fb81531801a88b2d8500563bd8281
AlexMtzGzn <alex13141926@gmail.com>
2025-06-21 19:18:50 -0600

    Ajuste , solo vamos a ajecutar

---
+++ VRP_TW/build/configuracion_json.d
@@ -0,0 +1,3 @@
+build/configuracion_json.o: src/configuracion_json.c \
+ src/../include/estructuras.h
+src/../include/estructuras.h:
---
+++ VRP_TW/build/movimientos_sa.d
@@ -0,0 +1,10 @@
+build/movimientos_sa.o: src/movimientos_sa.c src/../include/estructuras.h \
+ src/../include/control_memoria.h src/../include/../include/estructuras.h \
+ src/../include/lista_ruta.h src/../include/lista_flota.h \
+ src/../include/salida_datos.h
+src/../include/estructuras.h:
+src/../include/control_memoria.h:
+src/../include/../include/estructuras.h:
+src/../include/lista_ruta.h:
+src/../include/lista_flota.h:
+src/../include/salida_datos.h:
