7d0ed4ccfb1aef662efb2fcb60da4a9e4bb11135
JaimeLopezLara <lolj80@gmail.com>
2025-11-16 12:36:44 -0600

    Se eliminan 103,208,,205

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -2,18 +2,7 @@
 
 for i in {1..5}; do
     echo "Ejecución $i"
-    ./main 5 5 C103 25
-    ./main 5 5 C105 25
-    ./main 5 5 C106 25
-    ./main 5 5 C107 25
-    ./main 5 5 C108 25
-
-    ./main 5 5 C201 25
-    ./main 5 5 C202 25
-    ./main 5 5 C203 25
-    ./main 8 10 C204 25
-    ./main 5 5 C205 25
-    ./main 5 5 C206 25
-    ./main 5 5 C207 25
-    ./main 5 5 C208 25
+    ./main 8 10 C103 25
+    ./main 8 10 C205 25
+    ./main 8 10 C208 25
 done
