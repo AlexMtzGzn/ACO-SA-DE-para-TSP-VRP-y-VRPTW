f6a2da5c138f3f4f53750186ce28bdfd17185395
JaimeLopezLara <lolj80@gmail.com>
2025-11-15 21:19:50 -0600

    Se suben algunas uestras

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-for i in {1..1}; do
+for i in {1..5}; do
     echo "Ejecución $i"
-    ./main 3 1 R202 100
+    ./main 5 5 C109 25
+    ./main 5 5 C103 25
+    ./main 5 5 C105 25
+    ./main 5 5 C106 25
+    ./main 5 5 C107 25
+    ./main 5 5 C108 25
+
+    ./main 5 5 C201 25
+    ./main 5 5 C202 25
+    ./main 5 5 C203 25
+    ./main 8 10 C204 25
+    ./main 5 5 C205 25
+    ./main 5 5 C206 25
+    ./main 5 5 C207 25
+    ./main 5 5 C208 25
 done
