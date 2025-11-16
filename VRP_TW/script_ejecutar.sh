8d4719dcac9342cb3c5c80208bd7586627e133a7
JaimeLopezLara <lolj80@gmail.com>
2025-11-16 15:21:16 -0600

    Nuevos Datos

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-for i in {1..5}; do
+for i in {1..2}; do
     echo "Ejecución $i"
     ./main 8 10 C103 25
-    ./main 8 10 C205 25
-    ./main 8 10 C208 25
 done
+
+
+for i in {1..3}; do
+    echo "Ejecución $i"
+    ./main 15 15 C205 25
+done
+
+
+for i in {1..4}; do
+    echo "Ejecución $i"
+    ./main 15 15 C208 25
+done
+
