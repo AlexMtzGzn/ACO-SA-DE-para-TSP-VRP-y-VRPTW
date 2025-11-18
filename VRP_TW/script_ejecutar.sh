51cf1a42cd079de004382a3bef7ae4056982c9af
JaimeLopezLara <lolj80@gmail.com>
2025-11-17 20:39:10 -0600

    Se suben

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-for i in {1..2}; do
-    echo "Ejecución $i"
-    ./main 8 10 C103 25
-done
-
-
 for i in {1..3}; do
     echo "Ejecución $i"
     ./main 15 15 C205 25
@@ -14,6 +7,6 @@ done
 
 for i in {1..4}; do
     echo "Ejecución $i"
-    ./main 15 15 C208 25
+    ./main 17 15 C208 25
 done
 
