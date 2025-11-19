677b99c0cb27179e95ada4ba8eb5e3fcd53c8ef4
JaimeLopezLara <lolj80@gmail.com>
2025-11-18 20:34:10 -0600

    Cambio

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+./main 10 5 C102 50
 
-for i in {1..5}; do
+for i in {1..3}; do
     echo "Ejecución $i"
     ./main 10 5 C101 50
-    ./main 10 5 C102 50
     ./main 10 5 C103 50
     ./main 10 5 C104 50
     ./main 10 5 C105 50
@@ -11,6 +11,13 @@ for i in {1..5}; do
     ./main 10 5 C102 50
     ./main 10 5 C108 50
     ./main 10 5 C109 50
-    ./main 20 25 C208 25
 done
+
+for i in {1..5}; do
+    echo "Ejecución $i"
+    ./main 10 5 C107 50
+    ./main 25 25  C208 25
+done
+
+
 
