54b05aad10cb47c1006a8885ee7d685057baacb7
AlexMtzGzn <alex13141926@gmail.com>
2025-11-19 16:19:31 -0600

    Prueba jalando

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
 for i in {1..3}; do
     echo "Ejecución $i"
-
+    ./main 10 10 R101 25
+    ./main 10 10 R102 25
+    ./main 10 10 R102 25
+    ./main 10 10 R104 25
+    ./main 10 10 R105 25
+    ./main 10 10 R106 25
+    ./main 10 10 R107 25
+    ./main 10 10 R108 25
+    ./main 10 10 R109 25
+    ./main 10 10 R110 25
+    ./main 10 10 R111 25
+    ./main 10 10 R112 25
 done
-
 
 
 
