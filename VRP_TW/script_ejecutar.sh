1511d18ec3a2ae880d2620e9dd3999567df66fd5
JaimeLopezLara <lolj80@gmail.com>
2025-11-20 11:54:21 -0600

    Nuevas

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-./main 10 5 C102 50
 
-for i in {1..3}; do
+./main 10 5 C101 50
+./main 10 5 C103 50
+./main 10 5 C104 50
+./main 10 5 C105 50
+./main 10 5 C106 50
+
+for i in {1..2}; do
     echo "Ejecución $i"
-    ./main 10 5 C101 50
-    ./main 10 5 C103 50
-    ./main 10 5 C104 50
-    ./main 10 5 C105 50
-    ./main 10 5 C106 50
-    ./main 10 5 C102 50
     ./main 10 5 C108 50
     ./main 10 5 C109 50
 done
