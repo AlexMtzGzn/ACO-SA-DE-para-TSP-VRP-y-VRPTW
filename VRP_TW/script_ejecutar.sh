418693143570feeec30cc8afd2bf739b2e2eb9d5
JaimeLopezLara <lolj80@gmail.com>
2025-11-22 11:46:25 -0600

    Ajustamos 100

--- VRP_TW/script_ejecutar.sh
+++ VRP_TW/script_ejecutar.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-./main 10 5 C101 50
-./main 10 5 C103 50
-./main 10 5 C104 50
-./main 10 5 C105 50
-./main 10 5 C106 50
-
 for i in {1..2}; do
     echo "Ejecución $i"
-    ./main 10 5 C108 50
-    ./main 10 5 C109 50
-done
-
-for i in {1..5}; do
-    echo "Ejecución $i"
-    ./main 10 5 C107 50
-    ./main 25 25  C208 25
+    ./main 5 10 C107 100 
 done
 
 
